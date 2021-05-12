43143942f71094a76dc637910d2e24eb0bd3b1a7
gomjellie <gomjellie@gmail.com>
2021-05-12 12:47:36 +0900

    summoner 검색창 ui 구현 (1번)

--- src/features/summoner/Summoner.tsx
+++ src/features/summoner/Summoner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
-
+import GGSvg from "../../assets/GG.svg";
 interface MatchParams {
   userName: string;
 }
@@ -8,9 +8,17 @@ interface MatchParams {
 const Summoner = ({ match }: RouteComponentProps<MatchParams>) => {
   const { userName } = match.params;
 
-  return <div className="Summoner">
-    hi {userName}
-  </div>;
+  return (
+    <div className="Summoner">
+      <div className="SearchBar">
+        <div className="SearchInputBox">
+          <input placeholder="소환사명, 챔피언..."></input>
+          <img height="14" src={GGSvg} alt="GG"/>
+        </div>
+      </div>
+      hi {userName}
+    </div>
+  );
 };
 
 export default Summoner;
