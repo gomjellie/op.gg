061e8768ec0f4d2ba86d6d2f8c8c99d4c624a59f
gomjellie <gomjellie@gmail.com>
2021-05-12 20:29:57 +0900

    챔피언별 승률 스타일 추가

--- src/features/summoner/Summoner.tsx
+++ src/features/summoner/Summoner.tsx
@@ -64,7 +64,42 @@ const Summoner = ({ match }: RouteComponentProps<MatchParams>) => {
               <div className="Tier">Unranked</div>
             </div>
           </div>
-          <div className="WinRateBox"></div>
+          <div className="ChampsWinRateBox">
+            <div className="LRContainer">
+              <div className="ChampsWinRateButton Selected">Champion Win ratio</div>
+              <div className="ChampsWinRateButton">Rank win rate per week</div>
+            </div>
+            <div className="WinRateItem">
+              <img className="ChampAvatar" src={AvatarImg} alt=""/>
+              <div className="TDContainer1">
+                <div className="Top1">신지드</div>
+                <div className="Down1">CS 67.8 (2.4)</div>
+              </div>
+              <div className="TDContainer2">
+                <div className="Top2">2.47 평점</div>
+                <div className="Down2">4.3 / 6.1 / 10.7</div>
+              </div>
+              <div className="TDContainer3">
+                <div className="Top3">69%</div>
+                <div className="Down3">35게임</div>
+              </div>
+            </div>
+            <div className="WinRateItem">
+              <img className="ChampAvatar" src={AvatarImg} alt=""/>
+              <div className="TDContainer1">
+                <div className="Top1">리산드라</div>
+                <div className="Down1">CS 67.8 (2.4)</div>
+              </div>
+              <div className="TDContainer2">
+                <div className="Top2">4.53 평점</div>
+                <div className="Down2">6.6 / 3.0 / 6.8</div>
+              </div>
+              <div className="TDContainer3">
+                <div className="Top3">71%</div>
+                <div className="Down3">34게임</div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="RealContent">
           <div className="QueueTypes">
