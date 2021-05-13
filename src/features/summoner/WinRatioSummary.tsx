fb9c97c7b37dcb83bf957347b652f9d643f13b9c
gomjellie <gomjellie@gmail.com>
2021-05-14 01:27:24 +0900

    소숫점 처리기 utils로 빼놓음

--- src/features/summoner/WinRatioSummary.tsx
+++ src/features/summoner/WinRatioSummary.tsx
@@ -1,25 +1,8 @@
 import React, { useCallback, useState } from "react";
 import { exampleMost } from "./most.t";
+import { toFloatPrecision } from "../../utils/numbers";
 
 type Tab = "Champion Win Ratio" | "Rank win rate per week";
-
-/**
- * 1 / 3 에 자릿수 2면
- * 0.33
- * @param numerator 분자
- * @param denominator 분모
- * @param precision 자릿수
- * @returns
- */
-const toFloatPrecision = (
-  numerator: number,
-  denominator: number,
-  precision: number = 1
-) => {
-  return (
-    Math.floor((numerator * 10 ** precision) / denominator) / 10 ** precision
-  );
-};
 
 const WinRatioSummary: React.FC = () => {
   const data = exampleMost;
---
+++ src/utils/numbers.ts
@@ -0,0 +1,21 @@
+
+
+/**
+ * 1 / 3 에 자릿수 2면
+ * 0.33
+ * @param numerator 분자
+ * @param denominator 분모
+ * @param precision 자릿수
+ * @returns
+ */
+ const toFloatPrecision = (
+  numerator: number,
+  denominator: number,
+  precision: number = 1
+) => {
+  return (
+    Math.floor((numerator * 10 ** precision) / denominator) / 10 ** precision
+  );
+};
+
+export { toFloatPrecision };
