28dec4645ea358b93895594c9746ee70d1f64699
gomjellie <gomjellie@gmail.com>
2021-05-14 01:25:35 +0900

    모스트 챔프 고정값으로 구현

--- src/features/summoner/Summoner.tsx
+++ src/features/summoner/Summoner.tsx
@@ -5,7 +5,7 @@ import QuickLook from "./QuickLook";
 import OverView from "./OverView";
 import GameRecord from "./GameRecord";
 import RankGameOverView from "./RankGameOverView";
-import AvatarImg from "../../assets/avatar.jpg";
+import WinRatioSummary from "./WinRatioSummary";
 
 interface MatchParams {
   userName: string;
@@ -22,42 +22,7 @@ const Summoner = ({ match }: RouteComponentProps<MatchParams>) => {
         <div className="SideContent">
           <RankGameOverView summonerName={userName} rankType="솔랭"/>
           <RankGameOverView summonerName={userName} rankType="자유 5:5 랭크"/>
-          <div className="ChampsWinRateBox">
-            <div className="LRContainer">
-              <div className="ChampsWinRateButton Selected">Champion Win ratio</div>
-              <div className="ChampsWinRateButton">Rank win rate per week</div>
-            </div>
-            <div className="WinRateItem">
-              <img className="ChampAvatar" src={AvatarImg} alt=""/>
-              <div className="TDContainer1">
-                <div className="Top1">신지드</div>
-                <div className="Down1">CS 67.8 (2.4)</div>
-              </div>
-              <div className="TDContainer2">
-                <div className="Top2">2.47 평점</div>
-                <div className="Down2">4.3 / 6.1 / 10.7</div>
-              </div>
-              <div className="TDContainer3">
-                <div className="Top3">69%</div>
-                <div className="Down3">35게임</div>
-              </div>
-            </div>
-            <div className="WinRateItem">
-              <img className="ChampAvatar" src={AvatarImg} alt=""/>
-              <div className="TDContainer1">
-                <div className="Top1">리산드라</div>
-                <div className="Down1">CS 67.8 (2.4)</div>
-              </div>
-              <div className="TDContainer2">
-                <div className="Top2">4.53 평점</div>
-                <div className="Down2">6.6 / 3.0 / 6.8</div>
-              </div>
-              <div className="TDContainer3">
-                <div className="Top3">71%</div>
-                <div className="Down3">34게임</div>
-              </div>
-            </div>
-          </div>
+          <WinRatioSummary />
         </div>
         <div className="RealContent">
           <div className="QueueTypes">
---
+++ src/features/summoner/WinRatioSummary.tsx
@@ -0,0 +1,108 @@
+import React, { useCallback, useState } from "react";
+import { exampleMost } from "./most.t";
+
+type Tab = "Champion Win Ratio" | "Rank win rate per week";
+
+/**
+ * 1 / 3 에 자릿수 2면
+ * 0.33
+ * @param numerator 분자
+ * @param denominator 분모
+ * @param precision 자릿수
+ * @returns
+ */
+const toFloatPrecision = (
+  numerator: number,
+  denominator: number,
+  precision: number = 1
+) => {
+  return (
+    Math.floor((numerator * 10 ** precision) / denominator) / 10 ** precision
+  );
+};
+
+const WinRatioSummary: React.FC = () => {
+  const data = exampleMost;
+  const [tab, setTab] = useState<Tab>("Champion Win Ratio");
+  const onChampWinRatioClick = useCallback(() => {
+    setTab("Champion Win Ratio");
+  }, []);
+  const onPerWeekClick = useCallback(() => {
+    setTab("Rank win rate per week");
+  }, []);
+
+  const tabViews = (
+    <div className="LRContainer">
+      <div className={`ChampsWinRateButton ${tab === "Champion Win Ratio" ? "Selected" : ""}`} onClick={onChampWinRatioClick}>
+        Champion Win ratio
+      </div>
+      <div className={`ChampsWinRateButton ${tab === "Rank win rate per week" ? "Selected": ""}`} onClick={onPerWeekClick}>
+        Rank win rate per week
+      </div>
+    </div>
+  );
+
+  if (tab === "Rank win rate per week") {
+    return (
+      <div className="ChampsWinRatioBox">
+        { tabViews }
+        {data.recentWinRate.map((item, idx) => (
+          <div className="ChampWinRatioItem" key={`ChampWinRatioWeek_${item.name}_${idx}`}>
+            <img className="ChampAvatar" src={item.imageUrl} alt="" />
+            <div className="ChampName">{item.name}</div>
+            <div className="WinRatio">
+              {toFloatPrecision(item.wins * 100, item.wins + item.losses)}%
+            </div>
+            <div className="WinLose">
+              <div className="Win" style={{ flex: item.wins }}>
+                {item.wins}승
+              </div>
+              <div className="Loss" style={{ flex: item.losses }}>
+                {item.losses}패
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="ChampsWinRatioBox">
+      { tabViews }
+      {data.champions.map((champ, idx) => {
+        const { kills, deaths, assists, games, wins } = champ;
+        const prefix = "ChampWinRatio";
+        return (
+          <div className="ChampWinRatioItem" key={`${prefix}${champ.name}${idx}`}>
+            <img className="ChampAvatar" src={champ.imageUrl} alt="" />
+            <div className="TDContainer1">
+              <div className="Top1">{champ.name}</div>
+              <div className="Down1">
+                CS {champ.cs} ({toFloatPrecision(champ.cs, champ.games)})
+              </div>
+            </div>
+            <div className="TDContainer2">
+              <div className="Top2">
+                {toFloatPrecision(kills + assists, deaths)}:1 평점
+              </div>
+              <div className="Down2">
+                {toFloatPrecision(kills, games)} /{" "}
+                {toFloatPrecision(deaths, games)} /{" "}
+                {toFloatPrecision(assists, games)}
+              </div>
+            </div>
+            <div className="TDContainer3">
+              <div className="Top3">
+                {toFloatPrecision(wins * 100, games, 0)}%
+              </div>
+              <div className="Down3">{games}게임</div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default WinRatioSummary;
