9c34114b6f2b02991fe4fb2befc9270d4897b722
gomjellie <gomjellie@gmail.com>
2021-05-16 18:53:02 +0900

    Summoner 페이지에 OPGG 로고 버튼추가해서 홈으로 돌아갈 수 있게함

--- src/features/summoner/SearchBar.tsx
+++ src/features/summoner/SearchBar.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router";
 import GGSvg from "../../assets/GG.svg";
+import OPGGSvg from "../../assets/opgglogo.svg";
 
 const SearchBar: React.FC = () => {
+  const history = useHistory();
+
   return (
     <div className="SearchBar">
+      <div className="OPGG">
+        <img height="20" src={OPGGSvg} alt="" onClick={() => history.push("/")} />
+      </div>
       <div className="SearchInputBox">
         <input placeholder="소환사명, 챔피언..."></input>
         <img height="14" src={GGSvg} alt="GG" />
