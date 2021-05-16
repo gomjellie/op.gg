f8cfb4fe27cd13da781c2090d8e451ad819d513d
gomjellie <gomjellie@gmail.com>
2021-05-16 21:33:52 +0900

    1번과제 Search History 완료

---
+++ src/components/SummonerSearchInput.tsx
@@ -0,0 +1,91 @@
+import React, { useRef, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
+import { fetchSummoner } from "../features/summoner/summonerSlice";
+import { fetchMost } from "../features/summoner/mostSlice";
+import { fetchMatches } from "../features/summoner/matchesSlice";
+import GGSvg from "../assets/GG.svg";
+import CloseIcon from "../assets/close.png";
+import useLocalStorage from "../hooks/useLocalStorage";
+import useOnClickOutside from "../hooks/useOnClickOutside";
+
+const SummonerSearchInput: React.FC = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [summonerName, setSummonerName] = useState("");
+  const [focused, setFocused] = useState(false);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const [serachHistory, setSearchHistory] = useLocalStorage<string[]>(
+    "searchs",
+    []
+  );
+
+  const onSearch = (query: string) => {
+    dispatch(fetchSummoner(query));
+    dispatch(fetchMatches(query));
+    dispatch(fetchMost(query));
+
+    setSearchHistory((prevHistory) => {
+      /**
+       * 히스토리에 최근 4개만 유지시킨다.
+       * 만약 이미 검색내역에 있으면 스택 맨위로 끌어올린다.
+       */
+      if (prevHistory.includes(query))
+        return prevHistory.filter((h) => h !== query).concat(query);
+      return prevHistory.slice(-3).concat(query);
+    });
+    history.push(`/summoner/userName=${query}`);
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      onSearch(summonerName);
+    }
+  };
+
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSummonerName(event.target.value);
+  };
+
+  const handleClickOutside = () => setFocused(false);
+  const handleClickInside = () => setFocused(true);
+
+  useOnClickOutside(wrapperRef, handleClickOutside);
+
+  const renderedSearchSuggests = (
+    <div className="SummonerSearchSuggestsContainer">
+      {serachHistory
+        .slice(-5)
+        .reverse()
+        .map((user) => {
+          return (
+            <div className="SummonerSearched">
+              <span className="SummonerName" onClick={() => onSearch(user)}>
+                {user}
+              </span>
+              <img className="DeleteHistoryBtn" src={CloseIcon} alt="GG" />
+            </div>
+          );
+        })}
+    </div>
+  );
+  return (
+    <div
+      className="SummonerSearchInputWrapper"
+      ref={wrapperRef}
+      onFocus={handleClickInside}
+    >
+      <div className="SummonerSearchInput">
+        <input
+          placeholder="Name1, Name2, ..."
+          onKeyDown={onKeyDown}
+          onChange={onChangeInput}
+        />
+        <img height="14" src={GGSvg} alt="GG" />
+      </div>
+      {focused ? renderedSearchSuggests : null}
+    </div>
+  );
+};
+
+export default SummonerSearchInput;
--- src/features/summoner/SearchBar.tsx
+++ src/features/summoner/SearchBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router";
-import GGSvg from "../../assets/GG.svg";
 import OPGGSvg from "../../assets/opgglogo.svg";
+import SummonerSearchInput from "../../components/SummonerSearchInput";
 
 const SearchBar: React.FC = () => {
   const history = useHistory();
@@ -9,11 +9,15 @@ const SearchBar: React.FC = () => {
   return (
     <div className="SearchBar">
       <div className="OPGG">
-        <img height="20" src={OPGGSvg} alt="" onClick={() => history.push("/")} />
+        <img
+          height="20"
+          src={OPGGSvg}
+          alt=""
+          onClick={() => history.push("/")}
+        />
       </div>
-      <div className="SearchInputBox">
-        <input placeholder="소환사명, 챔피언..."></input>
-        <img height="14" src={GGSvg} alt="GG" />
+      <div className="SearchInputContainer">
+        <SummonerSearchInput />
       </div>
     </div>
   );
