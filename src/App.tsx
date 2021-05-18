6d8e76a4af5fbacd151bc0a415ddcfb1dc0fa709
gomjellie <gomjellie@gmail.com>
2021-05-18 10:31:52 +0900

    footer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Summoner from "./features/summoner/Summoner";
 import Main from "./features/main/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" component={Main} />
         </Switch>
       </Router>
+      <Footer />
     </div>
   );
 }
